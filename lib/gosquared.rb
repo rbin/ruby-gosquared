38f84b5bd7870c7d72f205ad1b6ee2ede811d603
rbin <me@rbin.co>
2014-10-10 16:08:04 +0100

    Initial commit

--- lib/gosquared.rb
+++ lib/gosquared.rb
@@ -4,24 +4,24 @@ require_relative 'gosquared/exceptions'
 require 'rest-client'
 
 module GoSquared
-	class Client
-		attr_accessor :api_user, :api_key, :host, :endpoint
+  class Client
+    attr_accessor :api_user, :api_key, :host, :endpoint
 
-		def initialize(params = {})
-			@api_user   = params.fetch(:api_user, nil)
-			@api_key    = params.fetch(:api_key, nil)
-			@host       = params.fetch(:host, 'https://api.gosquared.com')
-			@endpoint   = params.fetch(:endpoint, '/endpoint/')
-			@conn       = params.fetch(:conn, create_conn)
-			yield self if block_given?
-			raise SendGrid::Exception.new('api_user and api_key are required') unless @api_user && @api_key
-		end
+    def initialize(params = {})
+      @api_user   = params.fetch(:api_user, nil)
+      @api_key    = params.fetch(:api_key, nil)
+      @host       = params.fetch(:host, 'https://api.gosquared.com')
+      @endpoint   = params.fetch(:endpoint, '/endpoint/')
+      @conn       = params.fetch(:conn, create_conn)
+      yield self if block_given?
+      raise SendGrid::Exception.new('api_user and api_key are required') unless @api_user && @api_key
+    end
 
 
-		private
+    private
 
-		def create_conn
-			@conn = RestClient::Resource.new(@host)
-		end
-	end
+    def create_conn
+      @conn = RestClient::Resource.new(@host)
+    end
+  end
 end
